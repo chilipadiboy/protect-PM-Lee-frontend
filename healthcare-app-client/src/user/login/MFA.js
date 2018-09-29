4981c6a14ee44e53ad749836c9acf5218be99284
Gng <gjhsmiles@gmail.com>
2018-09-29 21:41:59 +0800

    implement read after write and show connection successful message

--- healthcare-app-client/src/user/login/MFA.js
+++ healthcare-app-client/src/user/login/MFA.js
@@ -4,19 +4,26 @@ import { Button } from 'antd';
 import {sign, hash} from 'tweetnacl';
 
 const connectMessage = "Please connect your bluetooth tag for multi-factor authentication!";
+const successfulConnectMessage = "Connection was successful! Web has been verified!";
 const connectButton = "Connect";
 const absentButton = "No Tag";
 const keyPair = sign.keyPair();
+let writeChar, readChar, disconnectChar;
+let stopReading = false;
 //this should be retrieved from the server as
 // {publicKey: Uint8Array(32), secretKey: Uint8Array(32)}
 
 class MFA extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      stopReadingState: stopReading,
+    }
+
   }
 
   startConnection() {
-    let writeChar, readChar, disconnectChar;
+   let context = this;
     navigator.bluetooth.requestDevice({
       filters: [ {services:[0x2220]} ]
     })
@@ -43,10 +50,13 @@ class MFA extends Component {
             disconnectChar = char;
           }
         }
+        // readChar.addEventListener('characteristicvaluechanged',
+        //                        handleCharacteristicValueChanged);
         let encoder = new TextEncoder('utf-8');
         let randomNumGen = encoder.encode(Math.random().toString()); //to convert it into a UInt8Array for nacl to sign
         let messageHash = hash(randomNumGen);
-        let signature = sign.detached(messageHash, keyPair.secretKey);
+      //  let signature = sign.detached(messageHash, keyPair.secretKey);
+        let signature = new Uint8Array([94, 66, 51, 205, 6, 46, 244, 152, 203, 192, 223, 242, 77, 74, 207, 137, 228, 32, 180, 160, 105, 149, 54, 74, 169, 0, 233, 186, 62, 224, 90, 198, 44, 175, 142, 38, 179, 88, 39, 237, 46, 157, 204, 240, 161, 99, 162, 244, 98, 103, 25, 15, 98, 31, 158, 1, 110, 81, 156, 228, 193, 225, 62, 11]);
         let stringEnder = encoder.encode("//");
         let sendMsg = concatenate(Uint8Array, messageHash, signature, keyPair.publicKey, stringEnder);
         let numOfChunks = Math.ceil(sendMsg.byteLength / 20);
@@ -56,13 +66,28 @@ class MFA extends Component {
         for (let i=0; i< numOfChunks; i++) {
            prevPromise = prevPromise.then(function() {
              return writeChar.writeValue(msgChunks[i]).then(function() {
-               if (i == numOfChunks-1) {
-                 dis(disconnectChar);
-               }
+               if (i === numOfChunks-1) {
+                    wait(5000);
+                 var prevWhilePromise = Promise.resolve();
+                  for (let j=0; j< 3; j++) {
+                    prevWhilePromise = prevWhilePromise.then(function() {
+                      return readChar.readValue().then(value => {
+                        var value2 = new Uint8Array(value.buffer);
+                        let encoder = new TextEncoder('utf-8');
+                        let ack = encoder.encode("ACK");
+                        return writeChar.writeValue(ack).then(function() {
+                         if(j == 2) {
+                           context.setState({stopReadingState:true})
+                           dis(disconnectChar);
+                         }
+                        })
+                       })
+                    })
+                  }
+                }
              })
            })
-         }
-
+        }
 
   }).catch(error => {
     console.log("this is err" + error);
@@ -76,20 +101,36 @@ class MFA extends Component {
   render() {
     return (
       <div className="mfa-container">
-        <p> {connectMessage} </p>
-        <Button type="primary" className="mfa-button" onClick={this.startConnection}> {connectButton} </Button>
-        <Button type="default" onClick={this.skipConnection.bind(this)}> {absentButton} </Button>
+        {this.state.stopReadingState ?
+          <p> {successfulConnectMessage} </p>
+          :
+          <div>
+          <p> {connectMessage} </p>
+          <Button type="primary" className="mfa-button" onClick={this.startConnection.bind(this)}> {connectButton} </Button>
+          <Button type="default" onClick={this.skipConnection.bind(this)}> {absentButton} </Button>
+          </div>
+        }
       </div>
     );
   }
 }
+
+
+function wait(ms){
+   var start = new Date().getTime();
+   var end = start;
+   while(end < start + ms) {
+     end = new Date().getTime();
+  }
+}
+
 
 function splitByMaxLength(sendMsg, numOfChunks) {
     let chunks = new Array(numOfChunks);
     let i, j, k;
     for (i=0; i<numOfChunks; i++) {
       chunks[i] = new Uint8Array(20);
-      for (j=0, k=i*20; j<20, k<i*20+20; j++, k++) {
+      for (j=0, k=i*20; j<20 && k<i*20+20; j++, k++) {
           chunks[i][j] = sendMsg[k];
       }
     }
