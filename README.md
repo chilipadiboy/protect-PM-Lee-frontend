6d9d8fdf9b413dc96db07b569ea4ce668f9eb474
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2018-10-01 10:53:25 +0800

    add in-built local database H2 & default admin user

--- README.md
+++ README.md
@@ -5,20 +5,6 @@ Learning to be good Info-Sec students to protect PM Lee's data
 
 ## Developer Guide
 
-For deployment on local host (perhaps, for testing purposes), go to `local-deployment` branch. This branch uses H2 database (instead of MySQL) which is in-built and doesn't require additional configuration, whereas MySQL database on `master` branch requires an additional step of setting up a local MySQL database. 
-
-Simply **cherry-pick** this commit on top of the branch. Alternatively, you can branch off from `local-deployment` branch instead of `master` branch.
-
-After you're done with the changes that you've made, perform the following steps on Sourcetree to remove this commit from your PR (you shouldn't merge the H2 database configurations into master branch).
-
-1. Suppose you're on branch `tag-verification`.
-![alt text](img/initial_state.png "initial_state")
-1. Right-click `master` and select the interactive rebase option.
-![alt text](img/interactive_rebase_1.png "interactive_rebase_1")
-1. Click on the commit to be deleted and click on `Delete` and `Ok`.
-![alt text](img/interactive_rebase_2.png "interactive_rebase_2")
-1. Perform a force push and you're good to go. 
-
 ### Frontend
 To run the front-end, go to 'healthcare-app-client' and run:
 1. `yarn install`
--- backend-server/src/main/java/org/cs4239/team1/protectPMLeefrontendserver/ProtectPmLeeFrontendServerApplication.java
+++ backend-server/src/main/java/org/cs4239/team1/protectPMLeefrontendserver/ProtectPmLeeFrontendServerApplication.java
@@ -1,15 +1,26 @@
 package org.cs4239.team1.protectPMLeefrontendserver;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.TimeZone;
 
 import javax.annotation.PostConstruct;
 
+import org.cs4239.team1.protectPMLeefrontendserver.model.Gender;
+import org.cs4239.team1.protectPMLeefrontendserver.model.Role;
+import org.cs4239.team1.protectPMLeefrontendserver.model.User;
+import org.cs4239.team1.protectPMLeefrontendserver.repository.UserRepository;
 import org.cs4239.team1.protectPMLeefrontendserver.storage.FileStorageProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @EntityScan(basePackageClasses = {
@@ -21,6 +32,12 @@ import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 })
 public class ProtectPmLeeFrontendServerApplication {
 
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
 	@PostConstruct
 	void init() {
 		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
@@ -28,5 +45,20 @@ public class ProtectPmLeeFrontendServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProtectPmLeeFrontendServerApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+		    userRepository.save(new User("S1234567A",
+                    "admin",
+                    "admin@gmail.com",
+                    "61111111",
+                    "Admin's House",
+                    21,
+                    Gender.MALE,
+                    passwordEncoder.encode("admin"),
+                    new HashSet<>(Collections.singletonList(Role.ADMINISTRATOR))));
+		};
 	}
 }
