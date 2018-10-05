0a7f5e5aa19ec77489184d46c97d447d566298ff
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2018-10-05 11:46:32 +0800

    Teach FileController to download files

--- backend-server/src/main/java/org/cs4239/team1/protectPMLeefrontendserver/controller/FileController.java
+++ backend-server/src/main/java/org/cs4239/team1/protectPMLeefrontendserver/controller/FileController.java
@@ -1,10 +1,21 @@
 package org.cs4239.team1.protectPMLeefrontendserver.controller;
 
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.cs4239.team1.protectPMLeefrontendserver.payload.UploadFileResponse;
 import org.cs4239.team1.protectPMLeefrontendserver.service.FileStorageService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -31,5 +42,32 @@ public class FileController {
                 .toUriString();
 
         return new UploadFileResponse(fileName, fileDownloadUri, file.getContentType(), file.getSize());
+    }
+
+
+    @GetMapping("/download/{fileName:.+}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
+        Resource resource = fileStorageService.loadFileAsResource(fileName);
+        String contentType = getContentType(request, resource);
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
+    }
+
+    @GetMapping("/downloadAllPaths")
+    public List<String> downloadAllPaths() {
+        return fileStorageService.loadAllFilePaths();
+    }
+
+    private String getContentType(HttpServletRequest request, Resource resource) {
+        try {
+            return request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (IOException ex) {
+            logger.info("Could not determine file type.");
+        }
+
+        return "application/octet-stream";
     }
 }
--- backend-server/src/main/java/org/cs4239/team1/protectPMLeefrontendserver/service/FileStorageService.java
+++ backend-server/src/main/java/org/cs4239/team1/protectPMLeefrontendserver/service/FileStorageService.java
@@ -1,14 +1,22 @@
 package org.cs4239.team1.protectPMLeefrontendserver.service;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.PathMatcher;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import org.cs4239.team1.protectPMLeefrontendserver.exception.FileNotFoundException;
 import org.cs4239.team1.protectPMLeefrontendserver.exception.FileStorageException;
 import org.cs4239.team1.protectPMLeefrontendserver.storage.FileStorageProperties;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -41,5 +49,36 @@ public class FileStorageService {
         }
 
         return fileName;
+    }
+
+    public Resource loadFileAsResource(String fileName) {
+        try {
+            Path filePath = fileStorageLocation.resolve(fileName).normalize();
+            Resource resource = new UrlResource(filePath.toUri());
+            if (resource.exists()) {
+                return resource;
+            } else {
+                throw new FileNotFoundException("File not found " + fileName);
+            }
+        } catch (MalformedURLException ex) {
+            throw new FileNotFoundException("File not found " + fileName, ex);
+        }
+    }
+
+    /**
+     * Assumes that there are no folders in {@code fileStorageLocation}.
+     */
+    public List<String> loadAllFilePaths() {
+        PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:**.png");
+
+        try {
+            return Files.walk(fileStorageLocation)
+                    .map(Path::getFileName)
+                    .filter(matcher::matches)
+                    .map(Path::toString)
+                    .collect(Collectors.toList());
+        } catch (IOException ioe) {
+            throw new FileStorageException("Error in loading files");
+        }
     }
 }
