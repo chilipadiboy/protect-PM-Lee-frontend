77dd8961ce3ccd6d6625ab54607f58a0dd1be493
gjlam95 <gj_lam@hotmail.com>
2018-10-09 11:53:02 +0800

    remove function to change to lowerCase

--- healthcare-app-client/src/user/records/MyRecords.js
+++ healthcare-app-client/src/user/records/MyRecords.js
@@ -17,7 +17,7 @@ class MyRecords extends Component {
     }
 
     loadUserRecords(nric, role) {
-        getUserRecords(nric, role.toString().toLowerCase())
+        getUserRecords(nric, role.toString)
         .then(data =>
           this.setState({ data }))
         .catch(error => {
