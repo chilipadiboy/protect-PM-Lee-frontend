9b83a0207b26f768e8c7da0673a3703e9cd560bf
gjlam95 <gj_lam@hotmail.com>
2018-10-11 13:18:36 +0800

    default csv values to graph

---
+++ healthcare-app-client/src/util/CSVtoGraph.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {XYPlot, XAxis, YAxis, VerticalGridLines, HorizontalGridLines, LineSeries} from 'react-vis';
+
+const Chart = (props) => {
+  return (
+    <XYPlot
+      width={300}
+      height={300}>
+      <HorizontalGridLines />
+      <LineSeries
+      color="red"
+      data={[
+        {x: 1, y: 10},
+        {x: 2, y: 5},
+        {x: 3, y: 15}
+      ]}/>
+      <XAxis title="X" />
+      <YAxis />
+      </XYPlot>
+      );
+    /*
+    const dataArr = props.data.map((d)=> {
+        return {x: d.year + '/' + d.quarter,
+        y: parseFloat(d.count/1000)}
+    });
+
+    return (
+        <XYPlot
+            xType="ordinal"
+            width={1000}
+            height={500}>
+            <VerticalGridLines />
+            <HorizontalGridLines />
+            <XAxis title="Period of time(year and quarter)" />
+            <YAxis title="Number of pull requests (thousands)" />
+                <LineSeries
+                    data={dataArr}
+                    style={{stroke: 'violet', strokeWidth: 3}}/>
+        </XYPlot>
+    );
+    */
+}
+
+export default Chart;
