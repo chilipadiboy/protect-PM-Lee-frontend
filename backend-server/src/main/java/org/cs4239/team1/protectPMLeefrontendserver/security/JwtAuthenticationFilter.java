aeecccc949916abe224afc85179f813a3ba937ca
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2018-10-17 11:33:26 +0800

    Teach JwtAuthenticationFilter to verify JWT value in cookie
    The JWT is stored in the cookie. Also, in the future it will no longer be sent to the server through the Authorisation header.
    
    Let's teach JwtAuthenticationFilter to retrieve the JWT from the cookie and verify the value of the JWT.

--- backend-server/src/main/java/org/cs4239/team1/protectPMLeefrontendserver/security/JwtAuthenticationFilter.java
+++ backend-server/src/main/java/org/cs4239/team1/protectPMLeefrontendserver/security/JwtAuthenticationFilter.java
@@ -1,9 +1,11 @@
 package org.cs4239.team1.protectPMLeefrontendserver.security;
 
 import java.io.IOException;
+import java.util.stream.Stream;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -62,10 +64,15 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     }
 
     private String getJwtFromRequest(HttpServletRequest request) {
-        String bearerToken = request.getHeader("Authorization");
-        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
-            return bearerToken.substring(7, bearerToken.length());
+        Cookie[] cookies = request.getCookies();
+        if (cookies == null) {
+            return null;
         }
-        return null;
+
+        return Stream.of(cookies)
+                .filter(cookie -> cookie.getName().equals("testCookie"))
+                .map(Cookie::getValue)
+                .findFirst()
+                .orElse(null);
     }
 }
