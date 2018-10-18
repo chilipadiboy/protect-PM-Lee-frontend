8db09d1e1c913a2891e2b4aa2b5fa3611c455526
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2018-10-18 16:36:36 +0800

    Teach FileController to restrict file types to be uploaded

--- backend-server/src/main/java/org/cs4239/team1/protectPMLeefrontendserver/controller/FileController.java
+++ backend-server/src/main/java/org/cs4239/team1/protectPMLeefrontendserver/controller/FileController.java
@@ -1,10 +1,13 @@
 package org.cs4239.team1.protectPMLeefrontendserver.controller;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.io.FilenameUtils;
 import org.cs4239.team1.protectPMLeefrontendserver.payload.ApiResponse;
 import org.cs4239.team1.protectPMLeefrontendserver.service.FileStorageService;
 import org.slf4j.Logger;
@@ -12,6 +15,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,11 +33,17 @@ public class FileController {
 
     private static final Logger logger = LoggerFactory.getLogger(FileController.class);
 
+    private static final Collection<String> ALLOWED_FILE_TYPES = Arrays.asList("jpg", "png", "mp4");
+
     @Autowired
     private FileStorageService fileStorageService;
 
     @PostMapping("/upload")
     public ResponseEntity<ApiResponse> uploadFile(@RequestParam("file") MultipartFile file) {
+        if (!ALLOWED_FILE_TYPES.contains(FilenameUtils.getExtension(file.getOriginalFilename()))) {
+            return new ResponseEntity<>(new ApiResponse(false, "Invalid file type."), HttpStatus.BAD_REQUEST);
+        }
+
         String fileName = fileStorageService.storeFile(file);
 
         String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
