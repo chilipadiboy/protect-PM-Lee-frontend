7a98c961507ccc2085dc5ab4416dcc8f3d83b873
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2018-10-18 12:12:42 +0800

    Perform input verification for logging in and signing up

--- backend-server/src/main/java/org/cs4239/team1/protectPMLeefrontendserver/payload/SignUpRequest.java
+++ backend-server/src/main/java/org/cs4239/team1/protectPMLeefrontendserver/payload/SignUpRequest.java
@@ -3,44 +3,51 @@ package org.cs4239.team1.protectPMLeefrontendserver.payload;
 import java.util.List;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 import lombok.Getter;
 
 @Getter
 public class SignUpRequest {
-    @NotBlank
+	@NotBlank
     @Size(min = 9, max = 9)
     private String nric;
 
-    @NotBlank
-    @Size(max = 40)
+	@NotBlank
+    @Size(min = 1, max = 40)
     private String name;
 
     @NotBlank
-    @Size(max = 40)
+    @Size(min = 1, max = 40)
     @Email
     private String email;
 
     @NotBlank
-    @Size(max = 20)
+    @Size(min = 8, max = 15)
     private String phone;
 
     @NotBlank
-    @Size(max = 100)
+    @Size(min = 1, max = 100)
     private String address;
 
+    @Min(0)
+    @Max(110)
     private int age;
 
+	@NotBlank
+	@Pattern(regexp = "male|female")
     private String gender;
 
     @NotBlank
-    @Size(max = 100)
+    @Size(min = 8, max = 100)
     private String password;
 
     @NotNull
     @Size(min = 1)
-    private List<String> roles;
+    private List<@Pattern(regexp = "patient|therapist|administrator|researcher|external partner") String> roles;
 }
