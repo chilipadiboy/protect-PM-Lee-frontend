9f6e1d9c87b6aa7b7767cff3c30fa2f553b52899
Gng <gjhsmiles@gmail.com>
2018-10-19 12:50:37 +0800

    add logout method in usercontroller

--- backend-server/src/main/java/org/cs4239/team1/protectPMLeefrontendserver/controller/UserController.java
+++ backend-server/src/main/java/org/cs4239/team1/protectPMLeefrontendserver/controller/UserController.java
@@ -1,16 +1,24 @@
 package org.cs4239.team1.protectPMLeefrontendserver.controller;
 
+import java.util.stream.Stream;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.cs4239.team1.protectPMLeefrontendserver.model.User;
 import org.cs4239.team1.protectPMLeefrontendserver.payload.UserSummary;
 import org.cs4239.team1.protectPMLeefrontendserver.repository.RecordRepository;
 import org.cs4239.team1.protectPMLeefrontendserver.repository.UserRepository;
 import org.cs4239.team1.protectPMLeefrontendserver.security.CurrentUser;
 import org.cs4239.team1.protectPMLeefrontendserver.service.RecordService;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,5 +42,19 @@ public class UserController {
         return new UserSummary(currentUser.getNric(), currentUser.getName(),
                 currentUser.getSelectedRole().toString(),
                 currentUser.getPhone(), currentUser.getEmail());
+    }
+
+    @PostMapping("/user/logout")
+    public ResponseEntity logOutCurrentUser(HttpServletRequest req, HttpServletResponse res) {
+        Cookie[] cookies = req.getCookies();
+        Cookie cookieFound = Stream.of(cookies)
+                .filter(cookie -> cookie.getName().equals("testCookie"))
+                .findFirst()
+                .orElse(null);
+        cookieFound.setPath("/api");
+        cookieFound.setHttpOnly(true);
+        cookieFound.setValue("1");
+        res.addCookie(cookieFound);  //send overwritten cookie
+        return ResponseEntity.ok(HttpStatus.OK);
     }
 }
