0d5e9995012c7e30003a077ccb3278489d913f78
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2018-10-25 17:20:57 +0800

    ok

--- backend-server/src/main/java/org/cs4239/team1/protectPMLeefrontendserver/controller/RecordController.java
+++ backend-server/src/main/java/org/cs4239/team1/protectPMLeefrontendserver/controller/RecordController.java
@@ -7,7 +7,6 @@ import java.util.Set;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
-import javax.validation.Valid;
 import javax.validation.Validation;
 import javax.validation.Validator;
 
@@ -38,6 +37,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -65,7 +65,7 @@ public class RecordController {
     private static final Logger logger = LoggerFactory.getLogger(RecordController.class);
 
     @PostMapping("/create/")
-    public ResponseEntity<?> createRecord(@Valid String recordRequest,
+    public ResponseEntity<?> createRecord(@RequestPart(value = "recordRequest") String recordRequest,
             @RequestParam(value = "file", required = false) MultipartFile file) {
         try {
             RecordRequest recordRequest1 = validate(new ObjectMapper().readValue(recordRequest, RecordRequest.class));
