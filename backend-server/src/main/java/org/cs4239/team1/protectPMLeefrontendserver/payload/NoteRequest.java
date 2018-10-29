31e19690f244cddc1c2c865bc7b1a6dc8e976be6
HONGKAI <chk1994@gmail.com>
2018-10-29 11:36:09 +0800

    added input restriction

--- backend-server/src/main/java/org/cs4239/team1/protectPMLeefrontendserver/payload/NoteRequest.java
+++ backend-server/src/main/java/org/cs4239/team1/protectPMLeefrontendserver/payload/NoteRequest.java
@@ -1,11 +1,20 @@
 package org.cs4239.team1.protectPMLeefrontendserver.payload;
 
 import lombok.Getter;
+import lombok.NonNull;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Getter
 public class NoteRequest {
 
+    @NonNull
+    @NotBlank
+    @Size(min = 9, max = 9)
     private String patientNric;
 
+    @NonNull
+    @NotBlank
     private String noteContent;
 }
