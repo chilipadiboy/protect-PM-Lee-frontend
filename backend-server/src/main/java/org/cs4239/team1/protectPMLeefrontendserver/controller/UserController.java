0ef547c860324c38ae9bf37566bb5d03652f777d
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2018-10-29 14:14:01 +0800

    Simplify UserController

--- backend-server/src/main/java/org/cs4239/team1/protectPMLeefrontendserver/controller/UserController.java
+++ backend-server/src/main/java/org/cs4239/team1/protectPMLeefrontendserver/controller/UserController.java
@@ -9,40 +9,26 @@ import javax.servlet.http.HttpServletResponse;
 import org.cs4239.team1.protectPMLeefrontendserver.model.User;
 import org.cs4239.team1.protectPMLeefrontendserver.payload.ApiResponse;
 import org.cs4239.team1.protectPMLeefrontendserver.payload.UserSummary;
-import org.cs4239.team1.protectPMLeefrontendserver.repository.RecordRepository;
-import org.cs4239.team1.protectPMLeefrontendserver.repository.UserRepository;
 import org.cs4239.team1.protectPMLeefrontendserver.security.CurrentUser;
-import org.cs4239.team1.protectPMLeefrontendserver.service.RecordService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/user")
 public class UserController {
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private RecordRepository recordRepository;
-
-    @Autowired
-    private RecordService recordService;
-
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
-    @GetMapping("/user/me")
+    @GetMapping("/me")
     public UserSummary getCurrentUser(@CurrentUser User currentUser) {
         return new UserSummary(currentUser.getNric(), currentUser.getName(),
                 currentUser.getSelectedRole().toString(),
                 currentUser.getPhone(), currentUser.getEmail());
     }
 
-    @GetMapping("/user/logout")
+    @GetMapping("/logout")
     public ApiResponse logoutCurrentUser(HttpServletRequest req, HttpServletResponse res) {
         Cookie cookie = Stream.of(req.getCookies())
                 .filter(c -> c.getName().equals("testCookie"))
