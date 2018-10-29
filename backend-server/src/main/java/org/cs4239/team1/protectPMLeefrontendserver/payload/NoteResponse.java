0f289dbed22afe9a5292c6bb68b02c4afefc4f23
HONGKAI <chk1994@gmail.com>
2018-10-29 11:25:58 +0800

    Remove unnecessary constructor

--- backend-server/src/main/java/org/cs4239/team1/protectPMLeefrontendserver/payload/NoteResponse.java
+++ backend-server/src/main/java/org/cs4239/team1/protectPMLeefrontendserver/payload/NoteResponse.java
@@ -2,11 +2,9 @@ package org.cs4239.team1.protectPMLeefrontendserver.payload;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
-@NoArgsConstructor
 public class NoteResponse {
     private Long noteID;
     private String creatorNric;
