43e6e04390fbd0f456e7b287b8af3cb7ea6f2b08
gjlam95 <gj_lam@hotmail.com>
2018-10-31 23:51:05 +0800

    page for external partner to upload database

--- healthcare-app-client/src/user/external_partner/Uploaddatabase.js
+++ healthcare-app-client/src/user/external_partner/Uploaddatabase.js
@@ -1,98 +1,63 @@
 import React, { Component } from 'react';
-import { Form, Upload, Button, Layout, Icon } from 'antd';
-import { AUTH_TOKEN, API } from '../../constants/index.js'
+import { Layout, Upload, Button, Icon } from 'antd';
+import { API, AUTH_TOKEN } from '../../constants/index.js';
 import './Uploaddatabase.css';
 
-const FormItem = Form.Item;
+class External_upload_database extends Component {
 
-class UploadButton extends Component {
-  render() {
-    return (
-      <div>
-        <Button type="primary" htmlType="submit" icon="file-text" size="default">Upload</Button>
-      </div>
-    );
-  }
-}
-
-class UploadDataForm extends Component {
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.props.form.validateFields((err, values) => {
-      if (!err) {
-        console.log('Received values of form: ', values);
-      }
-    });
-  }
-
-  normFile = (e) => {
-    console.log('Upload event:', e);
-    if (Array.isArray(e)) {
-      return e;
+  constructor(props) {
+    super(props);
+    this.state = {
+      filelist: []
     }
-    return e && e.fileList;
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = (info) => {
+    let fileList = info.fileList;
+
+    fileList = fileList.map((file) => {
+      if (file.response) {
+        // Component will show file.url as link
+        file.url = file.response.fileDownloadUri
+      }
+      return file;
+    });
+
+      fileList = fileList.filter((file) => {
+        return true;
+      });
+
+    this.setState({ fileList });
   }
 
   render() {
+    const { Header, Content } = Layout;
     const props = {
-      action: API + "",
+      action: API + "/external/upload/" + this.state.fileList,
       headers: {
         SessionId: localStorage.getItem(AUTH_TOKEN),
         enctype: "multipart/form-data"
       },
+      withCredentials: 'include',
       onChange: this.handleChange,
       multiple: false
     };
-      const { Header, Content } = Layout;
-      const { getFieldDecorator } = this.props.form;
-      const formItemLayout = {
-        labelCol: { span: 8 },
-        wrapperCol: { span: 14 },
-      };
-
-      return (
-            <Layout className="layout">
-              <Header>
-                <div className="title">Upload Data</div>
-              </Header>
-              <Content>
-                <Form onSubmit={this.handleSubmit}>
-                  <FormItem
-                    {...formItemLayout}
-                    label="Hospital database file"
-                    >
-                    {getFieldDecorator('upload', {
-                      valuePropName: 'file',
-                      getValueFromEvent: this.normFile,
-                    })(
-                      <Upload {...props} name="logo" action="/upload.do" listType="picture">
-                        <Button>
-                          <Icon type="search" /> Click to select file
-                        </Button>
-                      </Upload>
-                    )}
-                  </FormItem>
-                </Form>
-              </Content>
-            </Layout>
-      );
+    return (
+      <Layout className="layout">
+      <Header>
+      <div className="title">Upload Database</div>
+      </Header>
+      <Content>
+      <Upload {...props} fileList={this.state.fileList}>
+        <Button>
+          <Icon type="upload" /> Upload
+        </Button>
+      </Upload>
+      </Content>
+      </Layout>
+    );
   }
-}
-
-class External_upload_database extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            user: this.props.currentUser,
-            isLoading: false
-        }
-    }
-
-    render() {
-      const WrappedForm = Form.create()(UploadDataForm);
-
-      return <WrappedForm />;
-    }
 }
 
 export default External_upload_database;
