7c696016371275493f548bca7a7e2c69b351ac23
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2018-11-01 22:36:03 +0800

    update signature

--- backend-server/src/main/java/org/cs4239/team1/protectPMLeefrontendserver/service/ExternalPartnerService.java
+++ backend-server/src/main/java/org/cs4239/team1/protectPMLeefrontendserver/service/ExternalPartnerService.java
@@ -56,7 +56,7 @@ public class ExternalPartnerService {
         switch (recordsType){
             case "records":
                 //check input headers are correct.
-                if (!bufferedReader.readLine().equals("type,subtype,title,document,patientIC,signature")){
+                if (!bufferedReader.readLine().equals("type,subtype,title,document,patientIC")){
                     throw new BadRequestException("Bad column headers");
                 }
                 String newRecord;
@@ -68,7 +68,6 @@ public class ExternalPartnerService {
                     String title = parts[2];
                     String document = parts[3];
                     String patientNric =parts[4];
-                    String signature = parts[5];
 
                     //No point taking in a record if it does not belong to any patient in this hospital
                     User patient = userRepository.findByNric(patientNric)
@@ -77,7 +76,7 @@ public class ExternalPartnerService {
                         throw new BadRequestException("User_" + patient.getNric() + " is not a patient!");
                     }
 
-                    recordRepository.save(new Record(type,subtype,title,document,patientNric,signature));
+                    recordRepository.save(new Record(type,subtype,title,document,patientNric,""));
                 }
                 break;
             case "users":
