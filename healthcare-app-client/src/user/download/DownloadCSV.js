b4f95f6a690a1518aa215784130e9c0a91fb1555
gjlam95 <gj_lam@hotmail.com>
2018-11-01 23:01:23 +0800

    update csv chart to show systolic and diastolic graphs

--- healthcare-app-client/src/user/download/DownloadCSV.js
+++ healthcare-app-client/src/user/download/DownloadCSV.js
@@ -12,6 +12,7 @@ class DownloadCSV extends Component {
       this.b64DecodeUnicode = this.b64DecodeUnicode.bind(this);
       this.createGraph = this.createGraph.bind(this);
       this.parseData = this.parseData.bind(this);
+      this.replaceAll = this.replaceAll.bind(this);
   }
 
   b64DecodeUnicode(str) {
@@ -21,17 +22,21 @@ class DownloadCSV extends Component {
   }
 
   createGraph(data) {
-    	var years = [];
-    	var silverMinted = ["Silver Minted"];
+    	var xAxis = [];
+    	var yAxis1 = ["Systolic"];
+      var yAxis2 = ["Diastolic"];
 
-    	for (var i = 1; i < data.length; i++) {
+    	for (var i = 0; i < data.length-1; i++) {
           if (data[i][0] !== undefined && data[i][0] !== null
-              && data[i][2] !== undefined && data[i][2] !== null) {
-              years.push(data[i][0]);
-              silverMinted.push(data[i][2]);
+              && data[i][1] !== undefined && data[i][1] !== null
+            && data[i][2] !== undefined && data[i][2] !== null) {
+              xAxis.push(data[i][0]);
+              yAxis1.push(data[i][1]);
+              yAxis2.push(data[i][2]);
           } else {
-              years.push(0);
-              silverMinted.push(0);
+              xAxis.push(0);
+              yAxis1.push(0);
+              yAxis2.push(0);
           }
     	}
 
@@ -39,13 +44,13 @@ class DownloadCSV extends Component {
           bindto: '#chart',
     	    data: {
     	        columns: [
-    	        	silverMinted
+    	        	yAxis1, yAxis2
     	        ]
     	    },
     	    axis: {
     	        x: {
     	            type: 'category',
-    	            categories: years,
+    	            categories: xAxis,
     	            tick: {
     	            	multiline: false,
                     	culling: {
@@ -70,10 +75,15 @@ class DownloadCSV extends Component {
       this.createGraph(results.data);
   }
 
+  replaceAll(str, find, replace) {
+    return str.replace(new RegExp(find, 'g'), replace);
+  }
+
   showOutput(filename) {
       downloadFile(filename)
       .then(response => {
-        this.parseData(this.b64DecodeUnicode(response));
+        const str = this.replaceAll(this.b64DecodeUnicode(response), "/", ",");
+        this.parseData(str);
       })
       .catch(error => {
         notification.error({
